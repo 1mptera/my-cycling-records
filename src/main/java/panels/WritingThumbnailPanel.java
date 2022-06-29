68d94948892de47fe5bee3567be7daeee156317e
Inwoo Hwang <hsjkdss228@naver.com>
2022-06-29 12:50:44 +0900

    WritingThumbnailPanel 생성자 및 생성자 실행 method 정의

--- src/main/java/panels/WritingThumbnailPanel.java
+++ src/main/java/panels/WritingThumbnailPanel.java
@@ -1,5 +1,33 @@
 package panels;
 
-public class WritingThumbnailPanel {
+import models.Writing;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class WritingThumbnailPanel extends JPanel {
+  public WritingThumbnailPanel(Writing writing) {
+    this.setLayout(new GridLayout(4, 1));
+
+    this.addWriterThumbnailLabel(writing.writer());
+    this.addSubjectThumbnailLabel(writing.subject());
+    this.addTitleThumbnailLabel(writing.title());
+    this.addDistanceThumbnailLabel(writing.distance());
+  }
+
+  private void addWriterThumbnailLabel(String writer) {
+    this.add(new JLabel(writer));
+  }
+
+  private void addSubjectThumbnailLabel(String subject) {
+    this.add(new JLabel(subject));
+  }
+
+  private void addTitleThumbnailLabel(String title) {
+    this.add(new JLabel(title));
+  }
+
+  private void addDistanceThumbnailLabel(String distance) {
+    this.add(new JLabel(distance));
+  }
 }
